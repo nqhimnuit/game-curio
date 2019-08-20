5480045567c2f3f54824ff77440e6d357bcabf12
Minh Quốc Nguyễn <minh.nguyen@adnovum.vn>
2019-08-20 10:56:56 +0700

    CURIO-0004: Curio Facelet 0.1 - add apache commons 3.

--- src/main/java/game/curio/CurioSearch.java
+++ src/main/java/game/curio/CurioSearch.java
@@ -5,6 +5,8 @@ import static java.lang.String.format;
 import javax.ejb.Stateless;
 import javax.inject.Named;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * @author AdNovum Informatik AG
  */
@@ -34,6 +36,9 @@ public class CurioSearch {
 	}
 
 	public void updateOutput() {
+		if (StringUtils.isBlank(input)) {
+			output = "Please enter a game title to search";
+		}
 		if (input.equalsIgnoreCase("darkest dungeon")) {
 			output = "You searched for \"Darkest Dungeon\", which is correct!";
 		}
