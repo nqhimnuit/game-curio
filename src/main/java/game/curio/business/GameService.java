946f562a1a51e7f406ce1bf6ca33b24e2919c08b
Minh Quốc Nguyễn <minh.nguyen@adnovum.vn>
2019-08-29 16:43:46 +0700

    CURIO-0006: EntityManager to perform database operations
    - implement logic to Read data from DB.

--- src/main/java/game/curio/business/GameService.java
+++ src/main/java/game/curio/business/GameService.java
@@ -1,8 +1,12 @@
 package game.curio.business;
 
 import javax.ejb.Stateless;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import game.curio.entities.GameEntity;
+import game.curio.entities.GameEntity_;
 
 /**
  * @author AdNovum Informatik AG
@@ -11,7 +15,11 @@ import game.curio.entities.GameEntity;
 public class GameService extends ServiceSupport {
 
 	public GameEntity getGameById(Long id) {
-		return em.find(GameEntity.class, id);
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<GameEntity> query = cb.createQuery(GameEntity.class);
+		Root<GameEntity> root = query.from(GameEntity.class);
+		query.where(cb.equal(root.get(GameEntity_.id), id));
+		return getSingleResult(em.createQuery(query).getResultList());
 	}
 
 }
--- src/main/java/game/curio/business/ServiceSupport.java
+++ src/main/java/game/curio/business/ServiceSupport.java
@@ -1,12 +1,18 @@
 package game.curio.business;
 
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * @author AdNovum Informatik AG
  */
 abstract class ServiceSupport {
+
+	private static final Logger LOG = LogManager.getLogger(ServiceSupport.class);
 
 	static final EntityManager em;
 
@@ -14,4 +20,17 @@ abstract class ServiceSupport {
 		em = Persistence.createEntityManagerFactory("CURIO-PU-MARIADB").createEntityManager();
 	}
 
+	<T> T getSingleResult(List<T> results) {
+		if (results.size() > 1) {
+			LOG.error("found more than 1 entities with same primary key");
+			return null;
+		}
+
+		if (results.size() == 0) {
+			LOG.info("found 0 entities");
+			return null;
+		}
+
+		return results.get(0);
+	}
 }
