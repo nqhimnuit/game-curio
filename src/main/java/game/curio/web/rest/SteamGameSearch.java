7a22a51614b00255df9a75460314905afb1877c9
Minh Quoc Nguyen <nqminhuit@gmail.com>
2019-09-05 23:38:38 +0700

    CURIO-0008: REST API to connect to Steam
    - try to deserialize json, but it didn't work!

--- src/main/java/game/curio/web/rest/SteamGameSearch.java
+++ src/main/java/game/curio/web/rest/SteamGameSearch.java
@@ -2,6 +2,7 @@ package game.curio.web.rest;
 
 import static javax.ws.rs.core.Response.Status.OK;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.ws.rs.GET;
@@ -14,6 +15,9 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import game.curio.web.rest.dto.JsonGame;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jsoup.Jsoup;
@@ -30,24 +34,30 @@ public class SteamGameSearch {
 
 	private static final Logger LOG = LogManager.getLogger(SteamGameSearch.class);
 
-	private static final String STEAM_SEARCH_API = "https://store.steampowered.com/search";
+	private static final String STEAM_SEARCH_URI = "https://store.steampowered.com/search";
 
-	private static final String STEAM_QUERY = "term";
+	private static final String STEAM_GAME_TITLE_QUERY = "term";
 
-	// https://store.steampowered.com/api/appdetails?appids=262060
+	private static final String STEAM_GAME_DETAILS_URI = "https://store.steampowered.com/api/appdetails?appids=262060";
+
+	private static final String STEAM_APP_ID_QUERY = "appids";
 
 	@GET
 	@Path("/search/{gameTitle}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response searchGame(@PathParam("gameTitle") String gameTitle) {
+	public Response searchGame(@PathParam("gameTitle") String gameTitle) throws IOException {
 		String htmlResult = getSteamSearchResultHtml(gameTitle);
 		List<String> appIds = getAppIds(htmlResult);
-		return Response.status(OK).entity(appIds).build();
+		String gameDetails = getGameDetailsInfo(appIds.get(0));
+		LOG.info("game details = {}", gameDetails);
+
+		JsonGame game = deserialize(gameDetails);
+		return Response.status(OK).entity(game).build();
 	}
 
 	private String getSteamSearchResultHtml(String gameTitle) {
 		Client client = ClientBuilder.newClient();
-		WebTarget steamTarget = client.target(STEAM_SEARCH_API).queryParam(STEAM_QUERY, gameTitle);
+		WebTarget steamTarget = client.target(STEAM_SEARCH_URI).queryParam(STEAM_GAME_TITLE_QUERY, gameTitle);
 		LOG.info("steam target uri = {}", steamTarget.getUri().toString());
 		return steamTarget.request(MediaType.TEXT_HTML).get(String.class);
 	}
@@ -63,6 +73,25 @@ public class SteamGameSearch {
 			}
 		}
 		return appIds;
+	}
+
+	private String getGameDetailsInfo(String appId) {
+		Client client = ClientBuilder.newClient();
+		WebTarget gameTarget = client.target(STEAM_GAME_DETAILS_URI).queryParam(STEAM_APP_ID_QUERY, appId);
+		return gameTarget.request(MediaType.APPLICATION_JSON).get(String.class);
+	}
+
+	/**
+	 * https://www.baeldung.com/jackson-nested-values
+	 */
+	private JsonGame deserialize(String jsonGame) throws IOException {
+		JsonNode jsonNode = new ObjectMapper().readTree(jsonGame);
+		JsonGame game = new JsonGame();
+		game.setTitle(jsonNode.get("name").textValue());
+		game.setPrice(jsonNode.get("final_formatted").doubleValue());
+		game.setDescription(jsonNode.get("short_description").textValue());
+		//game.setReleaseDate(jsonNode.get("date").textValue());
+		return game;
 	}
 
 }
---
+++ src/main/java/game/curio/web/rest/dto/JsonGame.java
@@ -0,0 +1,52 @@
+package game.curio.web.rest.dto;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * @author AdNovum Informatik AG
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class JsonGame {
+
+	private String title;
+
+	private Date releaseDate;
+
+	private String description;
+
+	private Double price;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+
+	public void setReleaseDate(Date releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+}
